fd95dc637a0194b737bed5648fb4cc8bb66f5116
Chris Chen <ccchrischen6@gmail.com>
2020-08-15 12:45:58 -0400

    commited on Aug 15th

--- ad-service/ad-common/src/main/java/com/chrischen/ad/advice/CommonResponseDataAdvice.java
+++ ad-service/ad-common/src/main/java/com/chrischen/ad/advice/CommonResponseDataAdvice.java
@@ -1,5 +1,7 @@
 package com.chrischen.ad.advice;
 
+import com.chrischen.ad.annotation.IgnoreResponseAdvice;
+import com.chrischen.ad.vo.CommonResponse;
 import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.converter.HttpMessageConverter;
@@ -12,10 +14,25 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
  * Created by Chris Chen
  */
 @RestControllerAdvice
+@SuppressWarnings("all")
 public class CommonResponseDataAdvice implements ResponseBodyAdvice<Object> {
     @Override
     public boolean supports(MethodParameter methodParameter,
                             Class<? extends HttpMessageConverter<?>> aClass) {
+
+        //if the class has the annotation "IgnoreResponseAdvice", we do not response
+        if (methodParameter.getDeclaringClass().isAnnotationPresent(
+                IgnoreResponseAdvice.class
+        )) {
+            return false;
+        }
+
+        //if the method has the annotation "IgnoreResponseAdvice", we do not response
+        if (methodParameter.getMethod().isAnnotationPresent(
+                IgnoreResponseAdvice.class
+        )) {
+            return false;
+        }
         return false;
     }
 
@@ -24,6 +41,21 @@ public class CommonResponseDataAdvice implements ResponseBodyAdvice<Object> {
                                   Class<? extends HttpMessageConverter<?>> aClass,
                                   ServerHttpRequest serverHttpRequest,
                                   ServerHttpResponse serverHttpResponse) {
-        return null;
+
+        CommonResponse<Object> response = new CommonResponse<>(0, "");
+
+        //if o is null
+        if (null == o) {
+            return response;
+        }
+        //if o is already type of CommonResponse
+        else if (o instanceof CommonResponse) {
+            response = (CommonResponse<Object>) o;
+        }
+        //o is a regular object
+        else {
+            response.setData(o);
+        }
+        return response;
     }
 }
