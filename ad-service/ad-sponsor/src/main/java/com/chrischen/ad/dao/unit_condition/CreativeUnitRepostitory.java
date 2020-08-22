9059ae7592d52bc14651d8932e1b294d1ad10539
Chris Chen <ccchrischen6@gmail.com>
2020-08-22 13:23:28 -0400

    commited on Aug 22nd

---
+++ ad-service/ad-sponsor/src/main/java/com/chrischen/ad/dao/unit_condition/CreativeUnitRepostitory.java
@@ -0,0 +1,10 @@
+package com.chrischen.ad.dao.unit_condition;
+
+import com.chrischen.ad.entity.unit_condition.CreativeUnit;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Chris Chen
+ */
+public interface CreativeUnitRepostitory extends JpaRepository<CreativeUnit, Long> {
+}
