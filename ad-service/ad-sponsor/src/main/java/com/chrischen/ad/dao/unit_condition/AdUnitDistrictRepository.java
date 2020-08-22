5abe3febf54394ccc337d2bf7ebb61e1f55810e9
Chris Chen <ccchrischen6@gmail.com>
2020-08-22 13:22:41 -0400

    commited on Aug 22nd

---
+++ ad-service/ad-sponsor/src/main/java/com/chrischen/ad/dao/unit_condition/AdUnitDistrictRepository.java
@@ -0,0 +1,10 @@
+package com.chrischen.ad.dao.unit_condition;
+
+import com.chrischen.ad.entity.unit_condition.AdUnitDistrict;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ * Created by Chris Chen
+ */
+public interface AdUnitDistrictRepository extends JpaRepository<AdUnitDistrict, Long> {
+}
