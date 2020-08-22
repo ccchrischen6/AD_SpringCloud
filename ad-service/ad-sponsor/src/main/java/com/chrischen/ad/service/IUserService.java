3b22c0384a9ea3fa8eca551874fe7d43bab538c4
Chris Chen <ccchrischen6@gmail.com>
2020-08-22 16:32:15 -0400

    commited on Aug 22nd

--- ad-service/ad-sponsor/src/main/java/com/chrischen/ad/service/IUserService.java
+++ ad-service/ad-sponsor/src/main/java/com/chrischen/ad/service/IUserService.java
@@ -1,8 +1,18 @@
 package com.chrischen.ad.service;
 
+import com.chrischen.ad.exception.AdException;
+import com.chrischen.ad.vo.CreateUserRequest;
+import com.chrischen.ad.vo.CreateUserResponse;
+
 /**
  * Created by Chris Chen
  */
 public interface IUserService {
-
+    /**
+     * create user
+     * @param request
+     * @return
+     * @throws AdException
+     */
+    CreateUserResponse createUser(CreateUserRequest request) throws AdException;
 }
