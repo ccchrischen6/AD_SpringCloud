11c7b34f1b2b8b870ea5372f8e20b3e483733896
Chris Chen <ccchrischen6@gmail.com>
2020-08-23 15:31:33 -0400

    commited on Aug 23rd

--- ad-service/ad-sponsor/src/main/java/com/chrischen/ad/service/IAdUnitService.java
+++ ad-service/ad-sponsor/src/main/java/com/chrischen/ad/service/IAdUnitService.java
@@ -1,12 +1,17 @@
 package com.chrischen.ad.service;
 
 import com.chrischen.ad.exception.AdException;
-import com.chrischen.ad.vo.AdUnitRequest;
-import com.chrischen.ad.vo.AdUnitResponse;
+import com.chrischen.ad.vo.*;
 
 /**
  * Created by Chris Chen
  */
 public interface IAdUnitService {
     AdUnitResponse createUnit(AdUnitRequest request) throws AdException;
+
+    AdUnitKeywordResponse createUnitKeyword(AdUnitKeywordRequest request) throws AdException;
+
+    AdUnitItResponse createUnitIt(AdUnitItRequest request) throws AdException;
+
+    AdUnitDistrictResponse createUnitDistrict(AdUnitDistrictRequest request) throws AdException;
 }
