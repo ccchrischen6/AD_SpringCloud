291db3d08359a7c8ce89646270daccd924bad695
Chris Chen <ccchrischen6@gmail.com>
2020-08-23 15:09:14 -0400

    commited on Aug 23rd

---
+++ ad-service/ad-sponsor/src/main/java/com/chrischen/ad/vo/AdUnitKeywordResponse.java
@@ -0,0 +1,17 @@
+package com.chrischen.ad.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Created by Chris Chen
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class AdUnitKeywordResponse {
+    private List<Long> id;
+}
