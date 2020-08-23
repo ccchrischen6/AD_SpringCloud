b94cc797cb78362e3d7bc99fbe2f6bab4ec426f9
Chris Chen <ccchrischen6@gmail.com>
2020-08-22 21:22:42 -0400

    commited on Aug 22nd

--- ad-service/ad-sponsor/src/main/java/com/chrischen/ad/service/IAdPlanService.java
+++ ad-service/ad-sponsor/src/main/java/com/chrischen/ad/service/IAdPlanService.java
@@ -1,7 +1,28 @@
 package com.chrischen.ad.service;
 
+import com.chrischen.ad.entity.AdPlan;
+import com.chrischen.ad.exception.AdException;
+import com.chrischen.ad.vo.AdPlanGetRequest;
+import com.chrischen.ad.vo.AdPlanRequest;
+import com.chrischen.ad.vo.AdPlanResponse;
+
+import java.util.List;
+
 /**
  * Created by Chris Chen
  */
 public interface IAdPlanService {
+    /**
+     *
+     * @param request
+     * @return
+     * @throws AdException
+     */
+    AdPlanResponse createAdPlan(AdPlanRequest request) throws AdException;
+
+    List<AdPlan> getAdPlanByIds(AdPlanGetRequest request) throws AdException;
+
+    AdPlanResponse updateAdPlan(AdPlanRequest request) throws AdException;
+
+    void deleteAdPlan(AdPlanRequest request) throws AdException;
 }
