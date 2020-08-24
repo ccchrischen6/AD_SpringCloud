5dddf05d463158ea73ad18d056cd9b1c40406ac8
Chris Chen <ccchrischen6@gmail.com>
2020-08-23 23:59:34 -0400

    commited on Aug 23rd

---
+++ ad-service/ad-sponsor/src/main/java/com/chrischen/ad/vo/CreativeUnitRequest.java
@@ -0,0 +1,27 @@
+package com.chrischen.ad.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+/**
+ * Created by Chris Chen
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreativeUnitRequest {
+
+    private List<CreativeUnitItem> unitItems;
+
+    @Data
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class CreativeUnitItem{
+        private Long creativeId;
+        private Long unitId;
+    }
+}
