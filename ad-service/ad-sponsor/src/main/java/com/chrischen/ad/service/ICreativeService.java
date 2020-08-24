cc47cd70851d44116daddeca796e3b4091c8c324
Chris Chen <ccchrischen6@gmail.com>
2020-08-23 23:53:51 -0400

    commited on Aug 23rd

--- ad-service/ad-sponsor/src/main/java/com/chrischen/ad/service/ICreativeService.java
+++ ad-service/ad-sponsor/src/main/java/com/chrischen/ad/service/ICreativeService.java
@@ -1,7 +1,12 @@
 package com.chrischen.ad.service;
 
+
+import com.chrischen.ad.vo.CreativeRequest;
+import com.chrischen.ad.vo.CreativeResponse;
+
 /**
  * Created by Chris Chen
  */
 public interface ICreativeService {
+    CreativeResponse createCreative(CreativeRequest request);
 }
---
+++ ad-service/ad-sponsor/src/main/java/com/chrischen/ad/vo/CreativeResponse.java
@@ -0,0 +1,17 @@
+package com.chrischen.ad.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Chris Chen
+ */
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreativeResponse {
+    private Long id;
+    private String name;
+}
