48aae255318def49174cd1cb766ebc600abe035b
Chris Chen <ccchrischen6@gmail.com>
2020-08-24 11:35:02 -0400

    commited on Aug 24th

---
+++ ad-service/ad-sponsor/src/main/java/com/chrischen/ad/controller/CreativeOPController.java
@@ -0,0 +1,33 @@
+package com.chrischen.ad.controller;
+
+import com.alibaba.fastjson.JSON;
+import com.chrischen.ad.exception.AdException;
+import com.chrischen.ad.service.ICreativeService;
+import com.chrischen.ad.vo.CreativeRequest;
+import com.chrischen.ad.vo.CreativeResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Chris Chen
+ */
+@Slf4j
+@RestController
+public class CreativeOPController {
+    private final ICreativeService creativeService;
+    @Autowired
+    public CreativeOPController(ICreativeService creativeService) {
+        this.creativeService = creativeService;
+    }
+
+    @PostMapping("/create/creative")
+    public CreativeResponse createCreative(@RequestBody CreativeRequest request) throws AdException
+    {
+        log.info("ad-sponsor: createCreative -> {}",
+                JSON.toJSONString(request));
+        return creativeService.createCreative(request);
+    }
+}
