1104f56e1ed1dfc6c38a9492784079f45d37a900
Chris Chen <ccchrischen6@gmail.com>
2020-08-30 19:17:43 -0400

    commited on Aug 30th

---
+++ ad-service/ad-search/src/main/java/SearchApplication.java
@@ -0,0 +1,10 @@
+import org.springframework.boot.SpringApplication;
+
+/**
+ * Created by Chris Chen
+ */
+public class SearchApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SearchApplication.class, args);
+    }
+}
