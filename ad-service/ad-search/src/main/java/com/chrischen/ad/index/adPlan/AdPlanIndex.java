a741a896495258a614fb806d69883b235f558303
Chris Chen <ccchrischen6@gmail.com>
2020-09-09 13:06:19 -0400

    commited on Sept 9th

--- ad-service/ad-search/src/main/java/com/chrischen/ad/index/adPlan/AdPlanIndex.java
+++ ad-service/ad-search/src/main/java/com/chrischen/ad/index/adPlan/AdPlanIndex.java
@@ -1,7 +1,57 @@
 package com.chrischen.ad.index.adPlan;
 
+import com.chrischen.ad.index.IndexAware;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
 /**
  * Created by Chris Chen
  */
-public class AdPlanIndex {
+@Slf4j
+@Component
+public class AdPlanIndex implements IndexAware<Long, AdPlanObject> {
+
+    private static Map<Long, AdPlanObject> objectMap;
+
+    static {
+        //do not use HashMap for thread safety
+        objectMap = new ConcurrentHashMap<>();
+    }
+
+    @Override
+    public AdPlanObject get(Long key) {
+        return objectMap.get(key);
+    }
+
+    @Override
+    public void add(Long key, AdPlanObject value) {
+        log.info("before add: {}", objectMap);
+        objectMap.put(key, value);
+        log.info("after add: {}", objectMap);
+    }
+
+    @Override
+    public void update(Long key, AdPlanObject value) {
+        log.info("before update: {}", objectMap);
+
+        AdPlanObject object = objectMap.get(key);
+        if(object == null) {
+            objectMap.put(key,value);
+        }
+        else{
+            object.update(value);
+        }
+
+        log.info("after update: {}", objectMap);
+    }
+
+    @Override
+    public void delete(Long key, AdPlanObject value) {
+        log.info("before delete: {}", objectMap);
+        objectMap.remove(key);
+        log.info("after delete: {}", objectMap);
+    }
 }
