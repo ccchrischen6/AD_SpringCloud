abce3e6a4d1787aaf55a82d8a9fdd74c9a4591f1
Chris Chen <ccchrischen6@gmail.com>
2020-09-10 12:08:07 -0400

    commited on Sept 10th

---
+++ ad-service/ad-search/src/main/java/com/chrischen/ad/index/adUnit/AdUnitObject.java
@@ -0,0 +1,41 @@
+package com.chrischen.ad.index.adUnit;
+
+import com.chrischen.ad.index.adPlan.AdPlanObject;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Chris Chen
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdUnitObject {
+    private Long unitId;
+    private Integer unitStatus;
+    private Integer positionType;
+    private Long planId;
+
+    private AdPlanObject adPlanObject;
+
+    void update(AdUnitObject newObject) {
+        if (null != newObject.getUnitId()) {
+            this.unitId = newObject.getUnitId();
+        }
+
+        if (null != newObject.getUnitStatus()) {
+            this.unitStatus = newObject.getUnitStatus();
+        }
+
+        if (null != newObject.getPositionType()) {
+            this.positionType = newObject.getPositionType();
+        }
+        if (null != planId) {
+            this.planId = newObject.getPlanId();
+        }
+        if (null != newObject.getAdPlanObject()) {
+            this.adPlanObject = newObject.getAdPlanObject();
+        }
+    }
+}
