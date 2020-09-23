9f40769c33e936fc4db9985989365c4ec242608f
Chris Chen <ccchrischen6@gmail.com>
2020-09-22 21:06:51 -0400

    commited on Sept 22nd

---
+++ ad-service/ad-sponsor/src/test/java/com/chrischen/ad/Application.java
@@ -0,0 +1,14 @@
+package com.chrischen.ad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by Chris Chen
+ */
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
