756eb2104acc0aafe8d7cef6d42cfa5b70d9451e
Chris Chen <ccchrischen6@gmail.com>
2020-09-22 21:39:18 -0400

    commited on Sept 22nd

--- ad-service/ad-sponsor/src/test/java/com/chrischen/ad/service/DumpDataService.java
+++ ad-service/ad-sponsor/src/test/java/com/chrischen/ad/service/DumpDataService.java
@@ -1,6 +1,8 @@
 package com.chrischen.ad.service;
 
+import com.alibaba.fastjson.JSON;
 import com.chrischen.ad.Application;
+import com.chrischen.ad.constant.CommonStatus;
 import com.chrischen.ad.dao.AdPlanRepository;
 import com.chrischen.ad.dao.AdUnitRepository;
 import com.chrischen.ad.dao.CreativeRepository;
@@ -8,16 +10,31 @@ import com.chrischen.ad.dao.unit_condition.AdUnitDistrictRepository;
 import com.chrischen.ad.dao.unit_condition.AdUnitItRepository;
 import com.chrischen.ad.dao.unit_condition.AdUnitKeywordRepository;
 import com.chrischen.ad.dao.unit_condition.CreativeUnitRepostitory;
+import com.chrischen.ad.dump.table.AdPlanTable;
+import com.chrischen.ad.dump.table.AdUnitTable;
+import com.chrischen.ad.entity.AdPlan;
+import com.chrischen.ad.entity.AdUnit;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.collections.CollectionUtils;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Chen
  */
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = {Application.class}, webEnvironment = SpringBootTest.WebEnvironment.NONE)
+@Slf4j
 public class DumpDataService {
 
     @Autowired
@@ -34,5 +51,69 @@ public class DumpDataService {
     private AdUnitItRepository itRepository;
     @Autowired
     private AdUnitKeywordRepository keywordRepository;
+
+
+    //save adPlanTable to a local file
+    private void dumpAdPlanTable(String fileName){
+        List<AdPlan> adPlans = planRepository.findAllByPlanStatus(
+                CommonStatus.VALID.getStatus()
+        );
+
+        if(CollectionUtils.isEmpty(adPlans)){
+            return;
+        }
+
+        List<AdPlanTable> planTables = new ArrayList<>();
+        adPlans.forEach(p -> planTables.add(
+                new AdPlanTable(
+                        p.getId(),
+                        p.getUserId(),
+                        p.getPlanStatus(),
+                        p.getStartDate(),
+                        p.getEndDate()
+                )
+        ));
+
+        Path path = Paths.get(fileName);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)){
+            for (AdPlanTable planTable : planTables){
+                writer.write(JSON.toJSONString(planTable));
+                writer.newLine();
+            }
+        } catch (IOException e){
+            log.error("dumpAdPlanTable error");
+        }
+    }
+
+    private void dumpAdUnitTable(String fileName){
+        List<AdUnit> adUnits = unitRepository.findAllByUnitStatus(
+                CommonStatus.VALID.getStatus()
+        );
+
+        if(CollectionUtils.isEmpty(adUnits)) {
+            return;
+        }
+
+        List<AdUnitTable> unitTables = new ArrayList<>();
+        adUnits.forEach(p -> unitTables.add(
+                new AdUnitTable(
+                        p.getId(),
+                        p.getUnitStatus(),
+                        p.getPositionType(),
+                        p.getPlanId()
+                )
+        ));
+
+        Path path = Paths.get(fileName);
+        try (BufferedWriter writer = Files.newBufferedWriter(path)){
+            for(AdUnitTable unitTable : unitTables){
+                writer.write(JSON.toJSONString(unitTable));
+                writer.newLine();
+            }
+        }
+        catch (IOException e){
+            log.error("dumpAdUnitTable error");
+        }
+    }
 
 }
