f7bc632036fca9cd411e21fc6a7167514cf46b98
Chris Chen <ccchrischen6@gmail.com>
2020-09-22 21:04:46 -0400

    commited on Sept 22nd

---
+++ ad-service/ad-common/src/main/java/com/chrischen/ad/dump/table/AdCreativeUnitTable.java
@@ -0,0 +1,16 @@
+package com.chrischen.ad.dump.table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Chris Chen
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdCreativeUnitTable {
+    private Long adId;
+    private Long unitId;
+}
