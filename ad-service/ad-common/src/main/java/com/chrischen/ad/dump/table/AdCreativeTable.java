01548f515091a614859c2effbf94bd104c4cad02
Chris Chen <ccchrischen6@gmail.com>
2020-09-22 20:37:36 -0400

    commited on Sept 22nd

---
+++ ad-service/ad-common/src/main/java/com/chrischen/ad/dump/table/AdCreativeTable.java
@@ -0,0 +1,21 @@
+package com.chrischen.ad.dump.table;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by Chris Chen
+ */
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AdCreativeTable {
+    private Long adId;
+    private String name;
+    private Integer type;
+    private Integer materialType;
+    private Integer height;
+    private Integer width;
+    private String adUrl;
+}
