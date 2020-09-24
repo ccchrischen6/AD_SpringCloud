e32adc54fb16b02a1789115d7b2001b470a4ff8e
Chris Chen <ccchrischen6@gmail.com>
2020-09-23 22:45:05 -0400

    commited on Sept 23nd

--- ad-service/ad-search/src/main/java/com/chrischen/ad/handler/AdLevelDataHandler.java
+++ ad-service/ad-search/src/main/java/com/chrischen/ad/handler/AdLevelDataHandler.java
@@ -1,6 +1,8 @@
 package com.chrischen.ad.handler;
 
+import com.alibaba.fastjson.JSON;
 import com.chrischen.ad.dump.table.AdCreativeTable;
+import com.chrischen.ad.dump.table.AdCreativeUnitTable;
 import com.chrischen.ad.dump.table.AdPlanTable;
 import com.chrischen.ad.dump.table.AdUnitTable;
 import com.chrischen.ad.index.DataTable;
@@ -11,41 +13,43 @@ import com.chrischen.ad.index.adUnit.AdUnitIndex;
 import com.chrischen.ad.index.adUnit.AdUnitObject;
 import com.chrischen.ad.index.creative.CreativeIndex;
 import com.chrischen.ad.index.creative.CreativeObject;
+import com.chrischen.ad.index.creativeUnit.CreativeUnitIndex;
+import com.chrischen.ad.index.creativeUnit.CreativeUnitObject;
 import com.chrischen.ad.mysql.constant.OpType;
+import com.chrischen.ad.utils.CommonUtils;
 import lombok.extern.slf4j.Slf4j;
 
 /**
  * Created by Chris Chen
- *
+ * <p>
  * add, update or delete index
- *
+ * <p>
  * indexes of level 2 are separated from other indexes
- *
  */
 
 @Slf4j
 public class AdLevelDataHandler {
-    private static <K,V> void handleBinLogEvent(
-            IndexAware<K,V> indexAware,
+    private static <K, V> void handleBinLogEvent(
+            IndexAware<K, V> indexAware,
             K key,
             V value,
             OpType type) {
-                switch (type){
-                    case ADD:
-                        indexAware.add(key, value);
-                        break;
-                    case UPDATE:
-                        indexAware.update(key, value);
-                        break;
-                    case DELETE:
-                        indexAware.delete(key, value);
-                        break;
-                    default:
-                        break;
-                }
+        switch (type) {
+            case ADD:
+                indexAware.add(key, value);
+                break;
+            case UPDATE:
+                indexAware.update(key, value);
+                break;
+            case DELETE:
+                indexAware.delete(key, value);
+                break;
+            default:
+                break;
+        }
     }
 
-    public static void handleLevel2(AdPlanTable planTable, OpType type){
+    public static void handleLevel2(AdPlanTable planTable, OpType type) {
         AdPlanObject planObject = new AdPlanObject(
                 planTable.getId(),
                 planTable.getUserId(),
@@ -62,7 +66,7 @@ public class AdLevelDataHandler {
         );
     }
 
-    public static void handleLavel2(AdCreativeTable creativeTable, OpType type){
+    public static void handleLavel2(AdCreativeTable creativeTable, OpType type) {
         CreativeObject creativeObject = new CreativeObject(
                 creativeTable.getAdId(),
                 creativeTable.getName(),
@@ -82,9 +86,9 @@ public class AdLevelDataHandler {
         );
     }
 
-    public static void handleLevel3(AdUnitTable unitTable, OpType type){
+    public static void handleLevel3(AdUnitTable unitTable, OpType type) {
         AdPlanObject planObject = DataTable.of(AdPlanIndex.class).get(unitTable.getPlanId());
-        if(planObject == null){
+        if (planObject == null) {
             log.error("error found in handleLevel 3, the planObject has not generated: {}",
                     unitTable.getPlanId());
             return;
@@ -104,6 +108,39 @@ public class AdLevelDataHandler {
                 unitObject,
                 type
         );
+    }
+
+    public static void handleLevel3(AdCreativeUnitTable creativeUnitTable, OpType type) {
+        if (type == OpType.UPDATE) {
+            log.error("creativeUnitTable not support update");
+            return;
+        }
+
+        AdUnitObject unitObject = DataTable.of(AdUnitIndex.class).get(creativeUnitTable.getUnitId());
+        CreativeObject creativeObject = DataTable.of(CreativeIndex.class).get(creativeUnitTable.getAdId());
+
+        if (unitObject == null || creativeObject == null) {
+            log.error("error found in handleLevel 3, the unitObject or the creativeObject has not generated: {}",
+                    JSON.toJSONString(creativeUnitTable));
+            return;
+        }
+
+        CreativeUnitObject creativeUnitObject = new CreativeUnitObject(
+                creativeUnitTable.getAdId(),
+                creativeUnitTable.getUnitId()
+        );
+
+        handleBinLogEvent(
+                DataTable.of(CreativeUnitIndex.class),
+                CommonUtils.stringConcat(
+                        creativeUnitObject.getAdId().toString(),
+                        creativeUnitObject.getUnitId().toString()
+                ),
+                creativeUnitObject,
+                type
+        );
+
+
     }
 }
 
